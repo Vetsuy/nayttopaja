544f199b994f60460c6be0c704752aa09473eaf5
Vetsuy <99177876+Vetsuy@users.noreply.github.com>
2022-06-02 14:45:42 +0300

    hgg

--- JavaScript.js
+++ JavaScript.js
@@ -2,17 +2,18 @@ function f2() {
     var submit = document.getElementById("submit").value;
     
       alert("Thanks for the submission");
-    }
+}
     
-    function f1() {
-    var email = document.getElementById("email").value;
-    var filter = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/;
+function f1() {
+var email = document.getElementById("email").value;
+var filter = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/;
     
-    if(!filter.test(email))
+if(!filter.test(email))
     
-    alert("Please Enter Valid Email.");
-    }
-    function main() {
-    f1();
-    f2();
-    }
+alert("Please Enter Valid Email.");
+}
+
+function main() {
+  f1();
+  f2();
+}
