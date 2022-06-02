2755e0faa8346c4dfabf4524822b78c592bee8ef
Vetsuy <99177876+Vetsuy@users.noreply.github.com>
2022-06-02 14:16:14 +0300

    linkit toimii

--- JavaScript.js
+++ JavaScript.js
@@ -1,5 +1,9 @@
 function f1() {
-    var x = document.getElementById("Submit");
-
-    alert("Thank you for your feedback");
+    var submit = document.getElementById("submit").value;
+    
+    alert("Thanks for the submition");
+}
+    
+function main() {
+f1();
 }
